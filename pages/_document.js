2e8ecc4e46e02f73903287fbfa457d8536f0edd2
Sean <SeanCoutinho@my.uopeople.edu>
2023-11-30 16:38:55 +0200

    script

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,20 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-WFSW5887E7"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-WFSW5887E7');
+              `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,20 +1,51 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import PageTitle from '../../components/pagetitle'
 import BlogList from '../../components/BlogList'
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/footer';
 import Scrollbar from '../../components/scrollbar'
 import Logo from '/public/images/logo.png'
+import Head from 'next/head';
 
 const BlogSingle = (props) => {
     return (
-        <Fragment>
-            <Navbar Logo={Logo}/>
-            <PageTitle pageTitle={'Latest News'} pagesub={'Blog'}/> 
-            <BlogList/>
-            <Footer/>
-            <Scrollbar/>
-        </Fragment>
+        <>
+            <Head>
+                {/* Primary Meta Tags */}
+                <title>Grace Life Foundation - Empowering Communities</title>
+                <meta name="description" content="Empowering communities through education, healthcare, and sustainable development initiatives. Join us in making a difference!" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:title" content="Grace Life Foundation - Empowering Communities" />
+                <meta property="og:description" content="Empowering communities through education, healthcare, and sustainable development initiatives. Join us in making a difference!" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.gracelifefoundation.com/" />
+                <meta property="og:image" content="https://www.gracelifefoundation.com/images/logo.png" />
+
+                {/* Twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Grace Life Foundation - Empowering Communities" />
+                <meta name="twitter:description" content="Empowering communities through education, healthcare, and sustainable development initiatives. Join us in making a difference!" />
+                <meta name="twitter:image" content="https://www.gracelifefoundation.com/images/gracelife-logo.png" />
+
+                {/* Other Meta Tags */}
+                <link rel="canonical" href="https://www.gracelifefoundation.com/" />
+                <link rel="icon" href="/favicon.ico" />
+
+                {/* Additional tags for SEO */}
+                <meta name="robots" content="index, follow" />
+                <meta name="keywords" content="grace life, foundation, community empowerment, education, healthcare, sustainable development" />
+            </Head>
+
+            <Fragment>
+                <Navbar Logo={Logo} />
+                <PageTitle pageTitle={'Latest News'} pagesub={'Blog'} />
+                <BlogList />
+                <Footer />
+                <Scrollbar />
+            </Fragment>
+        </>
     )
 };
 export default BlogSingle;
